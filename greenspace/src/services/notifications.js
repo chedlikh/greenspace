7e45bb05271a409c2a78c5e50af1e3356c0ce440
chedlikh <160616334+chedlikh@users.noreply.github.com>
2025-04-22 21:09:56 +0100

    Update notifications.js

--- greenspace/src/services/notifications.js
+++ greenspace/src/services/notifications.js
@@ -1,7 +1,7 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
 
-const API_BASE_URL = "http://localhost:8089";
+const API_BASE_URL = "http://greenspace.ddns.net:8089";
 
 // Fetch unread notifications
 export const fetchUnreadNotifications = async (userId, token) => {
